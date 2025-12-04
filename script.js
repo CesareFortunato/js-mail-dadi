d83e542ac33506977848b8db130ce82c231a8e56
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-04 14:11:36 +0100

    feat: dice exercise

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Nota:
 Non è necessario provvedere alla validazione delle email */
 
 
-const mailArr = ['a@gmail.com', 'b@gmail.com', 'c@gmail.com', 'd@gmail.com', 'e@gmail.com']
+/* const mailArr = ['a@gmail.com', 'b@gmail.com', 'c@gmail.com', 'd@gmail.com', 'e@gmail.com']
 
 let userMail = prompt('inserisci la tua mail');
 
@@ -26,4 +26,38 @@ if(userFound === true){
 }
 else{
     console.log("purtroppo la tua mail non è presente!");    
+} */
+
+/* Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+
+Consigli del giorno:
+scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
+javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
+si ma noi cosa vogliamo fare?
+torniamo a scrivere in italiano
+proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
+
+const userNumber = Math.floor(Math.random() * 6) + 1;
+const pcNumber = Math.floor(Math.random() * 6) + 1;
+
+console.log("Tiro del giocatore:", userNumber);
+console.log("Tiro del PC:", pcNumber);
+
+if(userNumber < pcNumber){
+    console.log("Hai perso!!");
+    
+}
+else if(userNumber > pcNumber){
+    console.log("Hai vinto!!");
+    
+}
+else{
+    console.log("Pari!");
+    
 }
