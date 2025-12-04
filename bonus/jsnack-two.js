1c74b3ee8f2db2108e24ab5d2cc9eb81e331c71b
CesareFortunato <cesare.fortunato89@gmail.com>
2025-12-04 14:29:17 +0100

    feat: complete structure + bonus 2/5

--- bonus/jsnack-two.js
+++ bonus/jsnack-two.js
@@ -2,3 +2,18 @@
 L’utente inserisce due parole in successione, con due prompt. 
 Il software stampa prima la parola più corta, poi la parola più lunga; */
 
+const firstWord = prompt('scrivi la prima parola');
+const secondWord = prompt('scrivi la seconda parola');
+
+const firstLength = parseInt(firstWord.length);
+const secondLength = parseInt(secondWord.length);
+
+if(firstLength > secondLength){
+    console.log('Questa è la seconda parola', secondWord);
+    console.log('Questa è la prima parola', firstWord);    
+}
+else{
+    console.log('Questa è la prima parola', firstWord);
+    console.log('Questa è la seconda parola', secondWord);  
+    
+}
